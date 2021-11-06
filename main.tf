89e85e07b19f2dc025eb92f83852fc4244bd0fa2
ElFlamoChico <merv.yan@gmail.com>
2021-11-06 11:59:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_windows_virtual_machine" "TFAzureMerv" {
   admin_username      = "adminuser"
   admin_password      = "Password@123"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    var.vm_nic
   ]
   
   os_disk {
