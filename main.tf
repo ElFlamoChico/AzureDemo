d7713e0bf80d8f923dc24a3e7f43dad18e38151d
ElFlamoChico <merv.yan@gmail.com>
2021-11-06 10:54:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,5 +26,25 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = "RG_TFAzureMerv"
+  location            = "southafricanorth"
+  size                = "B1s"
+  admin_username      = "adminuser"
+  admin_password      = "Password@123"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
 }
