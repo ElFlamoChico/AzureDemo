cf713ca6d7cc38030d5433721c56384e070c5342
ElFlamoChico <merv.yan@gmail.com>
2021-11-06 11:45:01 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,19 @@
-variable "region" {
+variable "location" {
   description = "Azure region"
   default     = "southafricanorth"
 }
 
-variable "instance_type" {
+variable "vm_size" {
   description = "Type of Azure VM to provision"
   default     = "B1s"
 }
 
-variable "instance_name" {
-  description = "VM name"
-  default     = "Provisioned by Terraform"
+variable "name" {
+  description = "Virtual Machine Name"
+  default     = "VM Provisioned by Terraform"
 }
 
-
-
-
+variable "resource_group_name" {
+  description = "Name of Azure Resource Group"
+  default = "RG Provisioned by Terraform"
+}
