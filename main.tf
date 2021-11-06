fe9bf212af682339a423e9e4b4b6a87a510a68af
ElFlamoChico <merv.yan@gmail.com>
2021-11-06 09:19:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "77355a38-f37c-40f7-97e9-31d73006cb8d"
+  client_id       = "0faf94ff-7867-47db-af37-9c944cfda10f"
+  client_secret   = "9c5bfc07-9535-4411-b9d7-050b5119aaf1"
+  tenant_id       = "7cae8df1-0356-4a3a-9904-46e6fc32ecc2"
 }
 
 resource "azurerm_resource_group" "rg" {
